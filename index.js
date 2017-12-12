bf35f0905a7400f8512bac31a5bc308c6bc02c57
Sabrina Jones <sabrina.jones87@gmail.com>
2017-12-12 18:41:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-findMatching(drivers, string){
-
-
+findMatching(collection, string){
+ for (const drivers of collection) {
+   if (drivers.favoriteColor === color) {
+     console.log(user.firstName);
+   }
+ }
 }
